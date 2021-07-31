91c559f5c5b8c9f169408a081f0622cd947a73a2
Guacha <guachamalinga@gmail.com>
2021-07-30 21:03:37 -0500

    Making headway in poke generation

--- dex/pokedex.py
+++ dex/pokedex.py
@@ -1,10 +1,48 @@
 import requests, sys, json
 import pokepy
 
+import random
+from pokepy.resources_v2 import NatureResource, PokemonResource, PokemonSpeciesResource
 class Pokedex:
+    #Gen8 Compliant
+    entries = 898
+    api = pokepy.V2Client(cache='in_disk', cache_location="./data")
     
-    def __init__(self):
-        self.entries = 0
-        self.species = []
-        self.abilities = []
-        self.api = pokepy.V2Client(cache='in_disk', cache_location="../data")
+    @staticmethod
+    def get_random_nature() -> NatureResource:
+        
+        num = random.randint(1,25)
+        return Pokedex.api.get_nature(num)
+    
+    @staticmethod
+    def get_random_poke(range: list = None) -> PokemonResource:
+        if range:
+            n = random.choice(range)
+        else:
+            n = random.choice(range(1,899))
+        
+        return Pokedex.api.get_pokemon(n)
+    
+    @staticmethod
+    def random_poke_in_region(region: str) -> PokemonResource:
+        if region:
+            ranges = {
+                'kanto': [x for x in range(1,152)],
+                'johto': [x for x in range(152, 252)],
+                'hoenn': [x for x in range(252, 387)],
+                'sinnoh': [x for x in range(387, 493)],
+                'unova': [x for x in range(494, 650)],
+                'kalos': [x for x in range(650, 721)],
+                'alola': [x for x in range(721, 810)] + [x for x in range(10091, 10093)] + [x for x in range(10100, 10116)],
+                'galar': [x for x in range(810, 899)] + [x for x in range(10158, 10178)],
+            }
+            return Pokedex.get_random_poke(ranges[region])
+
+    @staticmethod
+    def get_evolution_level(species: PokemonResource):
+        species = Pokedex.api.get_species(species.species.name)
+    
+    
+    @staticmethod
+    def get_evolution_chain(species: PokemonSpeciesResource):
+        
--- pokemon/poke.py
+++ pokemon/poke.py
@@ -1,12 +1,57 @@
-class Pokemon:
+from random import random
+from pokepy.resources_v2 import PokemonResource, NatureResource
+class Pokemon():
     
-    def __init__(self, species, stats, status, boosts, moves):
-        self.species = None
-        self.stats = {}
-        self.status = None
-        self.boosts = {}
-        self.moves = []
+    def __init__(
+        self,
+        species: PokemonResource,
+        stats: dict = None,
+        status = None,
+        boosts = None,
+        moves = None,
+        ivs = None,
+        evs = None,
+        nature = None,
+        exp = 0):
         
+        self.species: NatureResource = species
+        self.stats: dict = stats
+        
+        # Setting default IVs if necessary
+        if ivs:
+            self.ivs: dict = ivs
+        else:
+            self.ivs: dict = {
+                'hp': random.randint(0,31),
+                'atk': random.randint(0,31),
+                'def': random.randint(0,31),
+                'spa': random.randint(0,31),
+                'spd': random.randint(0,31),
+                'spe': random.randint(0,31)
+            }
+        
+        # Setting default EVs if necessary
+        if evs:
+            self.evs: dict = evs
+        else:
+            self.evs: dict = {
+                'hp': 0,
+                'atk':0,
+                'def':0,
+                'spa':0,
+                'spd':0,
+                'spe':0,            
+            }
+        
+        self.status = status
+        self.boosts = boosts
+        self.moves = moves
+    
+    def calculate_stats():
+        
+        pass
+    
+    # TODO: Implement method to generate wild pokemon  
     @staticmethod
-    def create_wild(species):
-        
+    def create_wild(species: PokemonResource, level_range: list = None):
+        pass
